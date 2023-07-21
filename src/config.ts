7851fe574edff666eccb28aeb4d94fcd2be76f14
David Cuthbert <dacut@kanga.org>
2023-07-21 01:28:56 -0700

    Use Octokit to find release tarball.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 import fetch from 'node-fetch';
 import parseArgsStringToArgv from 'string-argv';
 import { ActionInputs } from "./args";
+import { Octokit } from 'octokit';
+import { createActionAuth } from '@octokit/auth-action';
 
 export type OutputType = "Json" | "Toml" | "Stdout" | "Xml" | "Html" | "Lcov";
 
@@ -39,12 +41,7 @@ export interface TarpaulinConfig {
  * @param input The parameters of the action.
  */
 export default async function resolveConfig(input: ActionInputs): Promise<TarpaulinConfig> {
-    let releaseEndpoint = 'https://api.github.com/repos/xd009642/tarpaulin/releases';
-    if (process.env.GITHUB_RELEASE_ENDPOINT) {
-        releaseEndpoint = process.env.GITHUB_RELEASE_ENDPOINT;
-    }
-
-    const downloadUrl = await getDownloadUrl(releaseEndpoint, input.requestedVersion);
+    const downloadUrl = await getDownloadUrl(input.requestedVersion);
     const type = input.runType ? input.runType : null;
     const timeout = input.timeout ? input.timeout : null;
     const outType = input.outType ? input.outType : "Xml";
@@ -67,24 +64,34 @@ export default async function resolveConfig(input: ActionInputs): Promise<Tarpau
 /**
  * Determine the download URL for the tarball containing the `cargo-tarpaulin` binaries.
  *
- * @param releaseEndpoint The URI of the GitHub API that can be used to fetch release information, sans the version number.
  * @param requestedVersion The Git tag of the tarpaulin revision to get a download URL for. May be any valid Git tag,
  * or a special-cased `latest`.
  */
-async function getDownloadUrl(releaseEndpoint: string, requestedVersion: string): Promise<string> {
-    const releaseInfoUri = requestedVersion === 'latest' ?
-        `${releaseEndpoint}/latest` :
-        `${releaseEndpoint}/tags/${requestedVersion}`;
+async function getDownloadUrl(requestedVersion: string): Promise<string> {
+    const auth = createActionAuth();
+    const creds = await auth();
+    const client = new Octokit({ auth: creds.token });
 
-    const releaseInfoRequest = await fetch(releaseInfoUri);
-    const releaseInfo = await releaseInfoRequest.json();
-    const asset = releaseInfo["assets"].find(asset => {
-        return asset['content_type'] === 'application/gzip';
-    });
+    var url: string | null;
 
-    if (!asset) {
-        throw new Error(`Couldn't find a release tarball containing binaries for ${requestedVersion}`);
+    if (requestedVersion == "latest") {
+        let response = await client.rest.repos.getLatestRelease({
+            owner: 'xd009642',
+            repo: 'tarpaulin',
+        });
+        url = response.data.tarball_url;
+    } else {
+        let response = await client.rest.repos.getReleaseByTag({
+            owner: 'xd009642',
+            repo: 'tarpaulin',
+            tag: requestedVersion,
+        });
+        url = response.data.tarball_url;
     }
 
-    return asset["browser_download_url"];
+    if (url == null) {
+        throw new Error(`Unable to find tarpaulin release tarball for version ${requestedVersion}`);
+    }
+
+    return url;
 }
